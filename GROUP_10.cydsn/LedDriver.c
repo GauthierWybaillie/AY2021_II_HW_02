7f261e4618aa40e45b62496f2d4274d989e0c859
Antonella <antonella.buquicchio@mail.polimi.it>
2021-04-04 21:23:12 +0200

    Header and source file for LED

---
+++ GROUP_10.cydsn/LedDriver.c
@@ -0,0 +1,51 @@
+/* ========================================
+ *
+ *
+*/
+
+#include "project.h"
+#include "LedDriver.h" //Header file
+
+extern volatile Color color;
+
+//Accessible only for this source file 
+static void RGBLed_WriteRed(uint8_t red);
+static void RGBLed_WriteGreen(uint8_t green);
+static void RGBLed_WriteBlue(uint8_t blue);
+
+//Implementation RGBLed_Start function: include the two PWM
+void RGBLed_Start(){
+    
+    PWM_RG_Start();
+    PWM_B_Start();
+}
+
+//Implementation RGBLed_Stop function: include the two PWM
+void RGBLed_Stop(){
+    
+    PWM_RG_Stop();
+    PWM_B_Stop();
+}
+
+//Switch on the red channel with the corresponding value
+static void RGBLed_WriteRed (uint8_t red){
+    PWM_RG_WriteCompare1(red);
+}
+
+//Switch on the green channel with the corresponding value
+static void RGBLed_WriteGreen (uint8_t green){
+    PWM_RG_WriteCompare2(green);
+}
+
+//Switch on the blue channel with the corresponding value
+static void RGBLed_WriteBlue (uint8_t blue){
+    PWM_B_WriteCompare(blue);
+}
+
+void RGBLed_WriteColor(Color c){
+    RGBLed_WriteRed(c.red);
+    RGBLed_WriteGreen(c.green);
+    RGBLed_WriteBlue(c.blue);
+}
+
+/* [] END OF FILE */
