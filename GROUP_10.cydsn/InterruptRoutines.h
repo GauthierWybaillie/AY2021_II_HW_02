c773d3868dbc73256c6d0def144bdaf381e7354b
Antonella <antonella.buquicchio@mail.polimi.it>
2021-04-04 21:24:28 +0200

    Header and Source files for Interrupt

---
+++ GROUP_10.cydsn/InterruptRoutines.h
@@ -0,0 +1,16 @@
+/* ========================================
+ *
+ * 
+*/
+
+// Declaration of ISR
+#ifndef __INTERRUPT_ROUTINES_H__
+   
+    #define __INTERRUPT_ROUTINES_H__
+    
+    #include "project.h"
+    
+    CY_ISR_PROTO(Custom_UART_RX_ISR);
+#endif
+
+/* [] END OF FILE */
