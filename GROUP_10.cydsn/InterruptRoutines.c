540acb13f151fbc9881b75a70e619a8267acebf4
Gauthier Wybaillie <gauthierguillaume.wybaillie@mail.polimi.it>
2021-04-07 12:10:39 +0200

    update code with flag, comments

--- GROUP_10.cydsn/InterruptRoutines.c
+++ GROUP_10.cydsn/InterruptRoutines.c
@@ -7,18 +7,22 @@
 #include "InterruptRoutines.h"
 #include "stdio.h"
 #include "UART.h"
+#include "Timer.h"
 
-extern volatile uint8_t status;
+
 extern volatile uint8_t packet;
+extern volatile uint8_t flag;
 
 CY_ISR(Custom_UART_RX_ISR){ 
     // Interrupt start if there is FIFO register not empty
     if (UART_ReadRxStatus() == UART_RX_STS_FIFO_NOTEMPTY){
+        
         /*
         We have some data in the FIFO: Read the RX data
-        */
+        */ 
+        flag = 1;
         packet = UART_ReadRxData();
-        status++;
+        Timer_Start();
         }    
     }
 
--- GROUP_10.cydsn/main.c
+++ GROUP_10.cydsn/main.c
@@ -6,7 +6,6 @@
 #include "project.h"
 #include "LedDriver.h"
 #include "InterruptRoutines.h"
-#include "stdio.h"
 #include "UART.h"
 
 #define Idle 0
@@ -19,83 +18,127 @@
 
 
 const Color BLACK = {0, 0, 0};
-volatile uint8_t status;
+
 volatile uint8_t packet;
-
-char message [20] = {'\0'};
-
-uint8_t start;    
+volatile uint8_t flag;
 volatile Color color;
+volatile uint8_t status;    
+
+uint8_t start;
+
 
 int main(void)
 {
-    CyGlobalIntEnable; /* Enable global interrupts. */
+    CyGlobalIntEnable;        /* Enable global interrupts. */
     
-    UART_Start();                               //Start UART periferal
+    //Start UART periferal
+    UART_Start();                               
     
+    // Start Led RGB
     RGBLed_Start();
-   
-    ISR_UART_StartEx(Custom_UART_RX_ISR);       // Initialization ISR
     
-    status = 0;                                 // Initialization of varial header to 0
+    // Initialization ISR due to UART
+    ISR_UART_StartEx(Custom_UART_RX_ISR);       
+    
+    // Initialization of the status to 0
+    status = Idle;                             
+    // Initialization of the variable start: it turns into one after the detection of the packet header
+    start = 0;                                
   
-    //Configuration Time for timeout -> just one time
+    RGBLed_WriteColor(BLACK); // RGB LED start in OFF condition 
+    
     
     for(;;)
     {
         switch (status){
+            // Idle status --> it waits the arrival of an Interrupt to switch status
             case Idle: 
-            RGBLed_WriteColor(BLACK); //Idle condition: LED OFF
             start = 0;
+            if (flag == 1){
+                status = Header;
+            }
             break;
             
-           case Header:
-            if(packet == 160){
+            /*
+            Header status -> Depending on the packet that is arrived from UART it can:
+              - Print the string required for the GUI;
+              - Switch to R (red) status if the Header packet is detected;
+              - Go back to Idle state and wait for the right packet. 
+            */
+            case Header: 
+            if (packet == 'v'){                          //Requirement for GUI
+                UART_PutString("RGB LED Program $$$");
+                status = Idle;
+                flag = 0 ;
+            }else if(packet == 160){                    // Control for Packet Header
                start = 1;
-               UART_PutString("Transmission started!!\r\n");
-            }else{
-                UART_PutString("Transmission NOT started!!\r\n");
+               status = R; 
+               flag = 0 ;
+            }else{ 
+                flag = 0 ;                              
                 status = Idle;
             }
             break;
             
+            /* 
+            R status -> If the packet header is detected and a data is arrived from UART,
+            the value is saved in the RED component of the struct color. Then it puts the flag
+            of the Interrupt to 0 and goes to the next status.
+            */
             case R:
-            if (start == 1){
+            if (start == 1 && flag == 1){
                 color.red = packet;
-                sprintf(message, "\nRed value: %c\r\n", color.red);
-                UART_PutString(message);
+                status = G;
+                flag = 0;
             }
             break;
             
+            /* 
+            G status -> If the packet header is detected and a data is arrived from UART,
+            the value is saved in the GREEN component of the struct color. Then it puts the flag
+            of the Interrupt to 0 and goes to the next status.
+            */
             case G:
-            if (start == 1){
+            if (start == 1 && flag == 1){
                 color.green = packet;
-                sprintf(message, "\nGreen value: %c\r\n", color.green); 
-                UART_PutString(message);
+                status = B;
+                flag = 0;
             }
             break;
             
+            /* 
+            B status -> If the packet header is detected and a data is arrived from UART,
+            the value is saved in the BLUE component of the struct color. Then it puts the flag
+            of the Interrupt to 0 and goes to the next status.
+            */
             case B:
-            if (start == 1){
+            if (start == 1 && flag == 1){
                 color.blue = packet;
-                sprintf(message, "\nBlue value: %c\r\n", color.blue); 
-                UART_PutString(message);
-            }
-            break;
-            
-            case Tail:
-            if (packet == 192){
-                status = LED;
-            }else{
-                UART_PutString("Invalid packet!!\r\n");
+                flag = 0;
                 status = Tail;
             }
             break;
             
+            /*
+            Tail status -> it waits the arrival of an interrupt and then controls if the 
+            data corresponds to the packet tail. If the transmission is completed, the status 
+            is switched to LED
+            */
+            case Tail:
+            if (flag == 1){
+                if (packet == 192){
+                    status = LED;
+                    flag = 0;
+                }else{
+                    status = Tail;
+                    flag = 0;
+                }
+            }
+            break;
+            
+            // LED status -> It turns on the LED and goes back to the IDLE status
             case LED:
             RGBLed_WriteColor(color);
-            CyDelay(1000);
-            UART_PutString("LED ACCESO!!\r\n");
             status = Idle;
             break;   
         }
